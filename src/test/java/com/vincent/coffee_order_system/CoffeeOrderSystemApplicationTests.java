cf2e5945f738c3137921f7fe585009ead59b712a
JINGSEN11 <1095027769@qq.com>
2026-09-10 14:57:42 +0800

    Initial commit

---
+++ src/test/java/com/vincent/coffee_order_system/CoffeeOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincent.coffee_order_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeOrderSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
